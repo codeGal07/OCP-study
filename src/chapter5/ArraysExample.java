37e05b8d7e8ee76438c923c274e0ce00485d7b46
sabinary <sabina.matjasic@gmail.com>
2022-02-15 22:55:47 +0100

    Add code examples for arrays

---
+++ src/chapter5/ArraysExample.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+import java.util.Arrays;
+
+public class ArraysExample {
+    public static void main(String[] args) {
+        String [] bugs = { "cricket", "beetle", "ladybug" };
+        System.out.println(Arrays.toString(bugs)); // since java 5 [cricket, beetle, ladybug]
+
+        String names[]; // points to null, array was never instantiated
+
+    }
+
+
+}
