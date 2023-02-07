706e6169ca4de8eca29f3d654db80aae6e467a9b
smatjasic <sabina.matjasic@gmail.com>
2023-02-07 10:29:22 +0100

    Add code examples

--- src/chapter4/Equality.java
+++ src/chapter4/Equality.java
@@ -11,12 +11,28 @@ public class Equality {
 
         var builder_a = new StringBuilder("a");
         var builder_b = new StringBuilder("a");
-
+        /*
+        We can use the equals() method for comparing two strings in Java since the String class overrides the equals() method
+        of the Object class, while StringBuilder doesn't override the equals() method of the Object class and hence equals()
+        method cannot be used to compare two StringBuilder objects
+         */
         System.out.println("builder equals: " + (builder_a).equals(builder_b)); //false (true only if passed the same object) compare objects
-        System.out.println("builder ==: " + ((builder_a) == (builder_b))); // false
+        System.out.println("builder ==: " + ((builder_a) == (builder_b))); // false (true only if passed the same object)
 
         System.out.println("builder to string equals: " + (builder_a.toString()).equals(builder_b.toString())); // true
         System.out.println("builder to string ==: " + (builder_a.toString()) == (builder_b.toString())); // false
+
+        System.out.println("------------------");
+
+        //String pool
+        var x = "Hello World";
+        var y = "Hello World";
+        System.out.println(x == y); // true
+
+        var xx = "Hello World";
+        var yy = "         Hello World".trim();
+        System.out.println(xx == yy); // false
+
     }
 
 
